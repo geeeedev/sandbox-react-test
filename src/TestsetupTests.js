fb8a1d77be1af988fc8e5db3df270ed141f4147e
Gwennie Lau <geeeedev@gmail.com>
2020-09-14 23:24:31 -0700

    ++

--- src/TestsetupTests.js
+++ src/TestsetupTests.js
@@ -1,5 +1,5 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
-// import once here or import in xxxxx.text.js file 
+// import once in setupTests.js or import in individual .text.js file 
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
