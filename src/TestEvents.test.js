9ecb391fa2893716d5707df0edb8f64059143e23
Gwennie Lau <geeeedev@gmail.com>
2020-09-16 21:27:50 -0700

    ++TestEvents

--- src/TestEvents.test.js
+++ src/TestEvents.test.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
-import TestEvents from './TestEvents';
-import '@testing-library/jest-dom/extend-expect'; 
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import TestEvents from "./components/TestEvents";
+import "@testing-library/jest-dom/extend-expect";
 
-  afterEach(cleanup);
-  
-  it('increments counter', () => {
-    const { getByTestId } = render(<TestEvents />); 
-    
-    fireEvent.click(getByTestId('button-up'))
+afterEach(cleanup);
 
-    expect(getByTestId('counter')).toHaveTextContent('1')
-  });
+//Test if increment/decrement events work correctly when click event is fired
 
-  
+it("Test Increment Counter", () => {
+  const { getByTestId } = render(<TestEvents />);
+  fireEvent.click(getByTestId("button-up"));
+  expect(getByTestId("counter")).toHaveTextContent('1');
+});
+
+it("Test Decrement Counter", ()=>{
+  const { getByTestId} = render(<TestEvents />);
+  fireEvent.click(getByTestId('button-down'));
+  expect(getByTestId('counter')).toHaveTextContent('-1');
+})
