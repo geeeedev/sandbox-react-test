f2e63e9557b5f72628f73ac58617befd4be258e8
Gwennie Lau <geeeedev@gmail.com>
2020-09-17 13:14:32 -0700

    ++TestAsync

--- src/TestAsync.test.js
+++ src/TestAsync.test.js
@@ -6,10 +6,16 @@ import "@testing-library/jest-dom/extend-expect";
 
 afterEach(cleanup);
 
-it('Test Increment Counter after 0.5 sec', async() => {                 //async()
+it('Test Increment Counter after 0.5 sec', async() => {               //async()
     const { getByTestId , getByText } = render(<TestAsync />);
     fireEvent.click(getByTestId('button-up'));
     // expect(getByTestId('counter')).toHaveTextContent('1');
     const delayedCounter = await waitForElement(()=>getByText('1'))   //await()
     expect(delayedCounter).toHaveTextContent('1');
 });
+
+
+//JS ES6 remember:
+//waitForElement(()=>getByText('1')) NOT waitForElement(()=>{getByText('1')})
+//either =>{return getByText(...)}) or =>getByText('1'))
+//{ } and return goes together, all or none
